c89be4dead4bebcad3ae7551e97ed1586cda2f5f
valosekj <jan.valosek@upol.cz>
2024-03-02 08:32:17 -0500

    On F10, fix selection in Czech

--- main.py
+++ main.py
@@ -25,9 +25,18 @@ Return only the corrected text, don't include a preamble.
 """
 )
 
+PROMPT_TEMPLATE_CZECH = Template(
+    """Oprav v následujícím textu napsaném v českém jazyce, všechny překlepy, velká písmena a interpunkci. 
+    Zachovej všechna obsažená slova a čísla. Nepřidávej nová slova a neměň pořadí slov. 
+    Zachovej všechny znaky nového řádku (takzvané EOL):
 
-def fix_text(text):
-    prompt = PROMPT_TEMPLATE.substitute(text=text)
+$text
+
+Vraťte pouze opravený text, nezahrnujte počáteční instrukce.
+"""
+)
+
+
 def fix_text_english(text):
     prompt = PROMPT_TEMPLATE_ENGLISH.substitute(text=text)
     response = httpx.post(
@@ -42,15 +51,18 @@ def fix_text_english(text):
     return response.json()["response"].strip()
 
 
-def fix_current_line():
-    # macOS short cut to select current line: Cmd+Shift+Left
-    controller.press(Key.cmd)
-    controller.press(Key.shift)
-    controller.press(Key.left)
-
-    controller.release(Key.cmd)
-    controller.release(Key.shift)
-    controller.release(Key.left)
+def fix_text_czech(text):
+    prompt = PROMPT_TEMPLATE_CZECH.substitute(text=text)
+    response = httpx.post(
+        OLLAMA_ENDPOINT,
+        json={"prompt": prompt, **OLLAMA_CONFIG},
+        headers={"Content-Type": "application/json"},
+        timeout=10,
+    )
+    if response.status_code != 200:
+        print("Error", response.status_code)
+        return None
+    return response.json()["response"].strip()
 
 
 def on_f9():
@@ -83,7 +95,32 @@ def on_f9():
 
 
 def on_f10():
-    fix_selection()
+    """
+    Fix selection in Czech
+    :return:
+    """
+    # 1. Copy selection to clipboard
+    with controller.pressed(Key.cmd):
+        controller.tap("c")
+
+    # 2. Get the clipboard string
+    time.sleep(0.1)
+    text = pyperclip.paste()
+
+    # 3. Fix string
+    if not text:
+        return
+    fixed_text = fix_text_czech(text)
+    if not fixed_text:
+        return
+
+    # 4. Paste the fixed string to the clipboard
+    pyperclip.copy(fixed_text)
+    time.sleep(0.1)
+
+    # 5. Paste the clipboard and replace the selected text
+    with controller.pressed(Key.cmd):
+        controller.tap("v")
 
 
 with keyboard.GlobalHotKeys({"<101>": on_f9, "<109>": on_f10}) as h:
